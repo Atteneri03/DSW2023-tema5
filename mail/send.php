7a42ebea444ea841546d59e19957a1da22ec1c80
Atteneri Estévez <atteeste2003@gmail.com>
2023-12-12 19:23:57 +0000

    send email

--- mail/send.php
+++ mail/send.php
@@ -1,4 +1,6 @@
 <?php
+require_once "../vendor/autoload.php";
+require_once "data.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 
@@ -6,14 +8,34 @@ $to = $_POST["to"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
+//Configuración del correo
 $mail = new PHPMailer();
 $mail->isSMTP();
-$mail->Host = "smtp.gmail.com";
+$mail->Host = $host;
 $mail->SMTPAuth = true;
-$mail->Username = "attenerie@gmail.com";
+$mail->Username = $user;
+$mail->Password = $pw;
+$mail->Port = 465;
+$mail->SMTPSecure = "ssl";
+$mail->setFrom($user, "Atteneri");
 
-echo "<p>$to</p>";
-echo "<p>$subject</p>";
-echo "<p>$message</p>";
+//El mensaje del correo
+$mail->addAddress($to);
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+if($mail->send()){
+  echo "<h2>Mensaje enviado correctamente</h2>";
+  echo "<p>Para: $to</p>";
+  echo "<p>Asunto: $subject</p>";
+  echo "<p>Mensaje: $message</p>";
+  
+} else {
+  echo "<h2>No se ha podido enviar el mensaje</h2>";
+}
+
+
+//Cerrar la conexión
+$mail->smtpClose();
 
 
